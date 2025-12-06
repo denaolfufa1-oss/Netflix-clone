b8359d7ad2f572057efba2f821709376e1577188
Denaol Fufa <denaolfufa1@gmail.com>
2025-12-06 17:50:04 +0300

    row

---
+++ netflix-clone/src/components/Rows/Row/Row.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './row.css';
+import axios from '../../../utils/axios';
+import requests from '../../../utils/requests';
+import { useEffect } from 'react';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
+const Row = ({title, fetchUrl, isLargeRow}) => {
+    const [movies, setMovies] = React.useState([]);
+    const [trailerUrl, setTrailerUrl] = React.useState("");
+    const base_url = "https://image.tmdb.org/t/p/original/";
+
+    useEffect(() => {
+        (async () => {
+            try {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            } catch (error) {
+                console.error("Error fetching movies:", error);
+            }
+        })();
+    }, [fetchUrl]);
+    const handleClick = (movie) => {
+        console.log(movie);
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || movie?.title || movie?.original_name )
+            .then((url) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            }).catch((error) => console.log(error));
+    }
+    };
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+                    
+    return (
+        <div className='row'>
+            <h1>{title}</h1>
+            <div className="row_posters">
+                {movies?.map((movie, index) => ( 
+                    <img 
+                    onClick={() => handleClick(movie)}
+                    key={index} src={`${base_url}${isLargeRow ? movie.poster_path: movie.backdrop_path}`} alt={`movie.name`} 
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    /> 
+                ))}
+            </div> 
+            <div style={{padding:'40px'}}>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+
+            </div>         
+        </div>
+    );
+}
+
+export default Row;
--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../../components/Footer/Footer.jsx';
 import Header from '../../components/Header/Header.jsx';
 import Banner from '../../components/Banner/Banner.jsx';
+import RowList from '../../components/Rows/RowList/RowList.jsx';
 
 const Home = () => {
     return (
       <>
         <Header></Header>
         <Banner></Banner>
+        <RowList></RowList>
         <Footer></Footer>
       </>
     );
