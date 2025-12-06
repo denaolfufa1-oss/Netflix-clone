7be20cc7cf1795623003fe76be038e752db7f327
Denaol Fufa <denaolfufa1@gmail.com>
2025-12-06 14:14:11 +0300

    banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer.jsx';
 import Header from '../../components/Header/Header.jsx';
+import Banner from '../../components/Banner/Banner.jsx';
 
 const Home = () => {
     return (
       <>
         <Header></Header>
+        <Banner></Banner>
         <Footer></Footer>
       </>
     );
